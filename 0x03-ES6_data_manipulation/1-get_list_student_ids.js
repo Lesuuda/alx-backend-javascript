5225f8b4b8be953e78dda50443f7249150835494
lesuuda <lesuudaljamani@gmail.com>
2024-07-01 10:52:22 +0300

    core: the use of array of objects and map function captured

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default function getListStudentIds(array) {
+    if (!Array.isArray(array)) {
+        return [];
+    }
+    return array.map((array) => array.id);
+    
+   
+
+}
