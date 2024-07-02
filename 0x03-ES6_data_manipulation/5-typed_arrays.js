cf27d32447d45a25e707a5ab0a4b1023921f2d72
lesuuda <lesuudaljamani@gmail.com>
2024-07-02 04:41:54 +0300

    core: typed arrays fix

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -7,5 +7,5 @@ export default function createInt8TypedArray(length, position, value) {
     const buffer = new ArrayBuffer(length);
     const int8View = new Int8Array(buffer);
     int8View[position] = value;
-    return {};
+    return buffer;
 }
