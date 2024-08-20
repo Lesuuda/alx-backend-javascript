13800a8c0dce138fbc2ff763bb36eb97e8b7d0af
lesuuda <lesuudaljamani@gmail.com>
2024-08-20 11:15:14 +0300

    fixed eslint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,10 +1,12 @@
+/* eslint-disable */
+
 const fs = require('fs');
 
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.trim().split('\n');
-
+    
     if (lines.length < 2) {
       throw new Error('Cannot load the database');
     }
@@ -12,10 +14,10 @@ function countStudents(path) {
     const fields = {};
     let totalStudents = 0;
 
-    for (let i = 1; i < lines.length; i++) { // Skip the header line
+    for (let i = 1; i < lines.length; i++) {
       const [firstname, , , field] = lines[i].split(',');
-
-      if (firstname && field) { // Ensure the line is valid (not empty)
+      
+      if (firstname && field) {
         totalStudents += 1;
         if (!fields[field]) {
           fields[field] = [];
