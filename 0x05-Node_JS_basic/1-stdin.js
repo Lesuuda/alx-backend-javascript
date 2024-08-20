04d4ffa163f46f40e818cc9ee8924ba1bc182eff
lesuuda <lesuudaljamani@gmail.com>
2024-08-20 11:58:04 +0300

    created a simple http server

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,17 @@
+/*eslint-disable */
+
 console.log('Welcome to Holberton School, what is your name?');
 if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
+    process.stdin.on('data', data => {
+        process.stdout.write(`Your name is: ${data.toString()}`);
+        process.exit();
+    })
 } else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
+    process.stdin.on('data', data => {
+        process.stdout.write(`Your name is: ${data.toString()}`);
+        process.exit();  
+    });
+    process.on('exit', () => {
+        process.stdout.write('This important software is now closing\n')
+    });
 }
